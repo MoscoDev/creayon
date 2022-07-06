7b5024936ef5c4e543379c4591174e93493cbee9
Audu Moses <89123661+MoscoDev@users.noreply.github.com>
2022-07-06 17:06:58 +0100

    profile page completed

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -4,55 +4,62 @@ import BottomNav from '../Components/BottomNav'
 import TopNav from '../Components/TopNav'
 import { BsCheck2Circle } from 'react-icons/bs'
 import { BiChevronRight } from 'react-icons/bi'
+import { FiEdit } from 'react-icons/fi'
+import { AiOutlineCreditCard } from 'react-icons/ai'
+import { TbDiscount2 } from 'react-icons/tb'
+import { MdNotificationsNone, MdOutlineLocationOn } from 'react-icons/md'
+
+
 
 function profile() {
   return (
     <div>
       <TopNav />
-      <div className="profile_page_content">
+      <div className={style["profile_page_content"]}>
         <div className={style.dpContainer}>
           <img src={"/img/avatar.svg"} alt="" height={"140px"} />
         </div>
+
         <div className={style.button}>
-          <BsCheck2Circle />
+          <BsCheck2Circle style={{ opacity: "0" }} />
           <div>
-            <p>Maria Salow</p>
-            <p>
-              <span>+23481634575</span>
-            </p>
+            <h3 className={style.userName}>Maria Salow</h3>
+
+            <small>+23481634575</small>
           </div>
-          
-          <BiChevronRight />
+
+          <FiEdit />
         </div>
+        <br />
         <div className={style.infoContainer}>
           <div className={style.button}>
-            <BsCheck2Circle />
+            <BsCheck2Circle size={"1.3rem"} className={style.icon} />
             <p>My Orders</p>
             <BiChevronRight />
           </div>
           <div className={style.button}>
-            <BsCheck2Circle />
+            <MdOutlineLocationOn size={"1.3rem"} className={style.icon} />
             <p>Address</p>
             <BiChevronRight />
           </div>
           <div className={style.button}>
-            <BsCheck2Circle />
+            <AiOutlineCreditCard size={"1.3rem"} className={style.icon} />
             <p>Payment</p>
             <BiChevronRight />
           </div>
           <div className={style.button}>
-            <BsCheck2Circle />
+            <TbDiscount2 size={"1.3rem"} className={style.icon} />
             <p>Voucher</p>
             <BiChevronRight />
           </div>
           <div className={style.button}>
-            <BsCheck2Circle />
+            <MdNotificationsNone size={"1.3rem"} className={style.icon} />
             <p>Notifications</p>
             <BiChevronRight />
           </div>
         </div>
       </div>
-      <BottomNav />
+      <BottomNav active={""} />
     </div>
   );
 }
