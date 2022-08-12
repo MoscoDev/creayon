8d4c77f6ee7bf8c62a3936545ecb9fc5f697f7eb
Audu Moses <89123661+MoscoDev@users.noreply.github.com>
2022-08-12 10:46:49 +0100

    persist data with redux-persist module

--- app/store.js
+++ app/store.js
@@ -1,11 +1,35 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../slices/counterSlice";
 import userReducer from "../slices/userSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+// import { PersistGate } from "redux-persist/integration/react";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    user: userReducer,
-  }
+  reducer: {user:persistedReducer, counter: counterReducer},
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 export default store;
--- pages/_app.js
+++ pages/_app.js
@@ -5,10 +5,12 @@ import '../styles/globals.css'
 import styles from "../styles/Home.module.css";
 import { store } from "../app/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import {persistStore} from "redux-persist";
 
 
 function MyApp({ Component, pageProps }) {
-  
+  let persistor = persistStore(store);
   return (
     <Layout>
       <Head>
@@ -19,7 +21,9 @@ function MyApp({ Component, pageProps }) {
         />
       </Head>
       <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
         <Component {...pageProps} />
+        </PersistGate>
       </Provider>
     </Layout>
   );
